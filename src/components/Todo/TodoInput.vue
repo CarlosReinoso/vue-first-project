c2d2261409c5e6dd25b7a59924e954cfe90a1bbb
Carlos Reinoso <carlos.reinoso@myraindrop.co.uk>
2023-10-01 21:47:35 +0100

    functionality to add items and remove them, and style the input tag

--- src/components/Todo/TodoInput.vue
+++ src/components/Todo/TodoInput.vue
@@ -1,5 +1,54 @@
-<script setup lang="ts"></script>
+<script>
+import { ref } from "vue";
+
+export default {
+  setup() {
+    const inputValue = ref("");
+    const itemList = ref([]);
+    console.log("🚀 ~ file: TodoInput.vue:9 ~ setup ~ itemList:", itemList);
+
+    const addItem = () => {
+      if (inputValue.value.trim() == "") return;
+      itemList.value.push(inputValue.value);
+      inputValue.value = "";
+    };
+
+    const deleteItem = (index) => {
+      console.log("🚀 ~ file: TodoInput.vue:17 ~ deleteItem ~ index:", index);
+      itemList.value.splice(index, 1);
+    };
+
+    return {
+      inputValue,
+      itemList,
+      addItem,
+      deleteItem,
+    };
+  },
+};
+</script>
 
 <template>
-  <h1>input</h1>
+  <h1>Todo</h1>
+  <input
+    v-model="inputValue"
+    @keyup.enter="addItem"
+    placeholder="Type and press enter"
+    class="custom-input"
+  />
+  <ul>
+    <li v-for="(item, index) in itemList" :key="index">
+      {{ item }}
+      <button @click="deleteItem(index)">Delete</button>
+    </li>
+  </ul>
 </template>
+
+<style>
+.custom-input {
+  border: 3px solid #42b883;
+  border-radius: 20px;
+  padding: 10px;
+  font-size: 16px;
+}
+</style>
--- src/components/Todo/TodoList.vue
+++ src/components/Todo/TodoList.vue
@@ -1,3 +1,3 @@
 <script setup lang="ts"></script>
 
-<template>todo list</template>
+<template></template>
